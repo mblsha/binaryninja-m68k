3f94092883c3803c519dc0db552ad77a4287b321
Michael Pishchagin <mblsha@google.com>
2022-06-26 16:03:41 +1000

    Add default calling convention where everything is passed via registers.

--- __init__.py
+++ __init__.py
@@ -16,6 +16,7 @@ log_debug(f'm68k Plugin loaded from: {os.path.dirname(__module__.__loader__.path
 
 from .m68k import *
 from .test import test_all
+from binaryninja import Architecture, CallingConvention
 
 #PluginCommand.register("Create M68k vector table", "Create M68k vector table", prompt_create_vector_table)
 PluginCommand.register_for_address("Create M68k vector table", "Create M68k vector table", prompt_create_vector_table)
@@ -30,6 +31,16 @@ M68LC040.register()
 M68EC040.register()
 M68330.register()
 M68340.register()
+
+# BinaryViewType['ELF'].register_arch(4, Endianness.BigEndian, Architecture['M68030'])
+BinaryViewType['ELF'].register_arch(4, Endianness.BigEndian, Architecture['M68030'])
+
+class ParametersInRegistersCallingConvention(CallingConvention):
+    name = "ParametersInRegisters"
+
+
+arch = Architecture['M68000']
+arch.register_calling_convention(ParametersInRegistersCallingConvention(arch, 'default'))
 
 BinaryViewType['ELF'].register_arch(4, Endianness.BigEndian, Architecture['M68030'])
 

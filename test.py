58307b4692f7a8af1b05984d53e1a17a929ebcfc
Michael Pishchagin <mblsha+gh@gmail.com>
2025-12-25 13:58:49 +1100

    Switch LLIL tests to structural mocks

--- test.py
+++ test.py
@@ -1,168 +1,198 @@
-# inspired by https://github.com/Vector35/arch-arm64/blob/staging/arm64test.py
-from .m68k import *
+from __future__ import annotations
 
-SAVE_FLAGS = ''
-RESTORE_FLAGS = ''
-FINALIZER = 'LLIL_RET(LLIL_POP.d())'
+import os
+from dataclasses import dataclass
 
-if RTS_PASS_FLAGS:
-    SAVE_FLAGS = 'LLIL_SET_REG.b(rc,LLIL_FLAG(c))'
-    RESTORE_FLAGS = '; LLIL_SET_FLAG(c,LLIL_REG.b(rc))'
-    FINALIZER = SAVE_FLAGS + '; ' + FINALIZER
+os.environ.setdefault("FORCE_BINJA_MOCK", "1")
 
-test_cases = [
+# Installs a stubbed `binaryninja` module into `sys.modules`.
+from binja_test_mocks import binja_api  # noqa: F401  # pyright: ignore
+from binja_test_mocks import mock_llil
+from binja_test_mocks.mock_llil import MockFlag, MockLLIL, MockReg, mllil, mreg
+
+from .m68k import RTS_PASS_FLAGS
+
+mock_llil.set_size_lookup(
+    {1: ".b", 2: ".w", 4: ".d", 8: ".q", 16: ".o"},
+    {"b": 1, "w": 2, "d": 4, "q": 8, "o": 16},
+)
+
+
+@dataclass(frozen=True)
+class LabelRef:
+    """Placeholder for a LowLevelILLabel (bound during assertion)."""
+
+    name: str
+
+
+def _l(name: str) -> LabelRef:
+    return LabelRef(name)
+
+
+def _flag(name: str) -> MockFlag:
+    return MockFlag(name)
+
+
+def _il(op: str, *ops: object) -> MockLLIL:
+    return mllil(op, list(ops))
+
+
+def _maybe_restore_flags() -> list[MockLLIL]:
+    if not RTS_PASS_FLAGS:
+        return []
+    return [_il("SET_FLAG", _flag("c"), _il("REG.b", mreg("rc")))]
+
+
+def _rts_expected() -> list[MockLLIL]:
+    out: list[MockLLIL] = []
+    if RTS_PASS_FLAGS:
+        out.append(_il("SET_REG.b", mreg("rc"), _il("FLAG", _flag("c"))))
+    out.append(_il("RET", _il("POP.d")))
+    return out
+
+
+test_cases: list[tuple[bytes, list[MockLLIL]]] = [
     # moveq     #$0000,d0
-    (b'\x70\x00', 'LLIL_SET_REG.d{nzvc}(d0,LLIL_CONST.d(0x0))'),
+    (b"\x70\x00", [_il("SET_REG.d{nzvc}", mreg("d0"), _il("CONST.d", 0))]),
 
     # subq.b    #$1,d0
     # FIXME: Generate flag 'x'
-    (b'\x53\x00', 'LLIL_SET_REG.b(d0.b,LLIL_SUB.b{*}(LLIL_REG.b(d0),LLIL_CONST.b(0x1)))'),
+    (
+        b"\x53\x00",
+        [
+            _il(
+                "SET_REG.b",
+                mreg("d0.b"),
+                _il("SUB.b{*}", _il("REG.b", mreg("d0")), _il("CONST.b", 1)),
+            )
+        ],
+    ),
 
     # jsr 0x5dc1c, no arguments for this call
-    (b'\x4e\xb9\x00\x05\xdc\x1c', 'LLIL_CALL(LLIL_CONST_PTR.d(0x5DC1C))' + RESTORE_FLAGS),
+    (b"\x4e\xb9\x00\x05\xdc\x1c", [_il("CALL", _il("CONST_PTR.d", 0x5DC1C))] + _maybe_restore_flags()),
 
     # at 0x53a, jsr 0x546, seems to be correctly interpreted as a call
-    (b'\x4e\xba\x00\x0a', 'LLIL_CALL(LLIL_CONST_PTR.d(0xC))' + RESTORE_FLAGS),
+    (b"\x4e\xba\x00\x0a", [_il("CALL", _il("CONST_PTR.d", 0xC))] + _maybe_restore_flags()),
 
     # lea (data_7a9ee[2]),a1
-    (b'\x43\xf9\x00\x07\xa9\xf0', 'LLIL_SET_REG.d(a1,LLIL_CONST_PTR.d(0x7A9F0))'),
+    (b"\x43\xf9\x00\x07\xa9\xf0", [_il("SET_REG.d", mreg("a1"), _il("CONST_PTR.d", 0x7A9F0))]),
 
     # tst.w     d1
-    (b'\x4a\x41', 'LLIL_SUB.w{nz}(LLIL_REG.w(d1),LLIL_CONST.w(0x0)); LLIL_SET_FLAG(v,LLIL_CONST.b(0x0)); LLIL_SET_FLAG(c,LLIL_CONST.b(0x0))'),
+    (
+        b"\x4a\x41",
+        [
+            _il("SUB.w{nz}", _il("REG.w", mreg("d1")), _il("CONST.w", 0)),
+            _il("SET_FLAG", _flag("v"), _il("CONST.b", 0)),
+            _il("SET_FLAG", _flag("c"), _il("CONST.b", 0)),
+        ],
+    ),
 
     # lea       ($279d2e),a0
     # move      $-004(a0),$0074(a6)
-    (b'\x41\xf9\x00\x27\x9d\x2e\x2d\x68\xff\xfc\x00\x74', 'LLIL_SET_REG.d(a0,LLIL_CONST_PTR.d(0x279D2E)); LLIL_STORE.d{nzvc}(LLIL_ADD.d(LLIL_REG.d(a6),LLIL_CONST.w(0x74)),LLIL_LOAD.d(LLIL_SUB.d(LLIL_REG.d(a0),LLIL_CONST.w(0x4))))'),
+    (
+        b"\x41\xf9\x00\x27\x9d\x2e\x2d\x68\xff\xfc\x00\x74",
+        [
+            _il("SET_REG.d", mreg("a0"), _il("CONST_PTR.d", 0x279D2E)),
+            _il(
+                "STORE.d{nzvc}",
+                _il("ADD.d", _il("REG.d", mreg("a6")), _il("CONST.w", 0x74)),
+                _il(
+                    "LOAD.d",
+                    _il("SUB.d", _il("REG.d", mreg("a0")), _il("CONST.w", 0x4)),
+                ),
+            ),
+        ],
+    ),
 
     # beq       (data_10)
-    (b'\x67\x00\x00\x0e', 'LLIL_IF(LLIL_FLAG_COND(0,None),1,3); LLIL_JUMP(LLIL_CONST_PTR.d(0x10))'),
+    (
+        b"\x67\x00\x00\x0e",
+        [
+            _il("IF", _il("FLAG_COND", 0, None), _l("t"), _l("f")),
+            _il("JUMP", _il("CONST_PTR.d", 0x10)),
+        ],
+    ),
 
     # jmp       ($4c862)
-    (b'\x4e\xf9\x00\x04\xc8\x62', 'LLIL_JUMP(LLIL_CONST_PTR.d(0x4C862))'),
+    (b"\x4e\xf9\x00\x04\xc8\x62", [_il("JUMP", _il("CONST_PTR.d", 0x4C862))]),
 
     # bra       (data_28)
-    (b'\x60\x00\x00\x26', 'LLIL_JUMP(LLIL_CONST_PTR.d(0x28))'),
+    (b"\x60\x00\x00\x26", [_il("JUMP", _il("CONST_PTR.d", 0x28))]),
 
     # dbf       d7,(data_-2c)
-    (b'\x51\xcf\xff\xd4','; '.join(['LLIL_IF(LLIL_CONST.b(0x0),6,1); ' + FINALIZER + '; LLIL_SET_REG.w(temp0,LLIL_SUB.w(LLIL_REG.w(d7),LLIL_CONST.w(0x1))); LLIL_SET_REG.w(d7.w,LLIL_REG.w(temp0)); LLIL_IF(LLIL_CMP_E.w(LLIL_REG.w(temp0),LLIL_CONST.w(0xFFFF)),6,4); LLIL_JUMP(LLIL_CONST_PTR.d(0xFFFFFFD6))'])),
+    (
+        b"\x51\xcf\xff\xd4",
+        [
+            _il("IF", _il("CONST.b", 0), _l("skip"), _l("decrement")),
+            _il(
+                "SET_REG.w",
+                mreg("TEMP0"),
+                _il("SUB.w", _il("REG.w", mreg("d7")), _il("CONST.w", 1)),
+            ),
+            _il("SET_REG.w", mreg("d7.w"), _il("REG.w", mreg("TEMP0"))),
+            _il(
+                "IF",
+                _il(
+                    "CMP_E.w",
+                    _il("REG.w", mreg("TEMP0")),
+                    _il("CONST.w", -1),
+                ),
+                _l("skip"),
+                _l("branch"),
+            ),
+            _il("JUMP", _il("CONST_PTR.d", -42)),
+        ],
+    ),
 
     # bcc       (data_5a)
-    (b'\x64\x00\x00\x58', 'LLIL_IF(LLIL_FLAG_COND(7,None),1,3); LLIL_JUMP(LLIL_CONST_PTR.d(0x5A))'),
+    (
+        b"\x64\x00\x00\x58",
+        [
+            _il("IF", _il("FLAG_COND", 7, None), _l("t"), _l("f")),
+            _il("JUMP", _il("CONST_PTR.d", 0x5A)),
+        ],
+    ),
 
     # rts
-    (b'\x4e\x75', ''),
+    (b"\x4e\x75", _rts_expected()),
 
     # andi.b    #$-2,ccr
-    (b'\x02\x3c\x00\xfe', 'LLIL_SET_FLAG(c,LLIL_CONST.b(0x0))'),
+    (b"\x02\x3c\x00\xfe", [_il("SET_FLAG", _flag("c"), _il("CONST.b", 0))]),
 
     # ori.b     #$1,ccr
-    (b'\x00\x3c\x00\x01', 'LLIL_SET_FLAG(c,LLIL_CONST.b(0x1)); LLIL_SET_FLAG(x,LLIL_CONST.b(0x1))'),
+    (
+        b"\x00\x3c\x00\x01",
+        [
+            _il("SET_FLAG", _flag("c"), _il("CONST.b", 1)),
+            _il("SET_FLAG", _flag("x"), _il("CONST.b", 1)),
+        ],
+    ),
 
     # scs.b     d1
-    (b'\x55\xc1', 'LLIL_IF(LLIL_FLAG_COND(3,None),1,3); LLIL_SET_REG.b(d1.b,LLIL_CONST.b(0x1)); LLIL_GOTO(5); LLIL_SET_REG.b(d1.b,LLIL_CONST.b(0x0)); LLIL_GOTO(5)'),
+    (
+        b"\x55\xc1",
+        [
+            _il("IF", _il("FLAG_COND", 3, None), _l("set"), _l("clear")),
+            _il("SET_REG.b", mreg("d1.b"), _il("CONST.b", 1)),
+            _il("GOTO", _l("skip")),
+            _il("SET_REG.b", mreg("d1.b"), _il("CONST.b", 0)),
+            _il("GOTO", _l("skip")),
+        ],
+    ),
 
     # swap      d6
-    (b'\x48\x46', 'LLIL_SET_REG.d(d6,LLIL_ROR.d(LLIL_REG.d(d6),LLIL_CONST.b(0x10)))'),
+    (
+        b"\x48\x46",
+        [_il("SET_REG.d", mreg("d6"), _il("ROR.d", _il("REG.d", mreg("d6")), _il("CONST.b", 0x10)))],
+    ),
+
+    # rtr
+    (
+        b"\x4e\x77",
+        [
+            _il("SET_REG.w", mreg("ccr"), _il("POP.w")),
+            _il("RET", _il("POP.d")),
+        ],
+    ),
 ]
 
-if RTS_PASS_FLAGS:
-    # rtr
-    test_cases.append((b'\x4e\x77', 'LLIL_SET_REG.w(ccr,LLIL_POP.w()); LLIL_RET(LLIL_POP.d())'))
-else:
-    # rtr
-    test_cases.append((b'\x4e\x77', 'LLIL_SET_REG.w(ccr,LLIL_POP.w())'))
-
-import re
-import sys
-import binaryninja
-from binaryninja import binaryview
-from binaryninja import lowlevelil
-from binaryninja.enums import LowLevelILOperation
-
-def il2str(il):
-    sz_lookup = {1:'.b', 2:'.w', 4:'.d', 8:'.q', 16:'.o'}
-    if isinstance(il, lowlevelil.LowLevelILInstruction):
-        size_code = sz_lookup.get(il.size, '?') if il.size else ''
-        flags_code = '' if not hasattr(il, 'flags') or not il.flags else '{%s}'%il.flags
-
-        # print size-specified IL constants in hex
-        if il.operation in [LowLevelILOperation.LLIL_CONST, LowLevelILOperation.LLIL_CONST_PTR] and il.size:
-            tmp = il.operands[0]
-            if tmp < 0: tmp = (1<<(il.size*8))+tmp
-            tmp = '0x%X' % tmp if il.size else '%d' % il.size
-            return '%s%s(%s)' % (il.operation.name, size_code, tmp)
-        else:
-            return '%s%s%s(%s)' % (il.operation.name, size_code, flags_code, ','.join([il2str(o) for o in il.operands]))
-    elif isinstance(il, list):
-        return '[' + ','.join([il2str(x) for x in il]) + ']'
-    else:
-        return str(il)
-
-def instr_to_il(data):
-    RETURN = b'\x4e\x75' # rts
-
-    platform = binaryninja.Platform['M68000']
-    # make a pretend function that returns
-    bv = binaryview.BinaryView.new(data + RETURN)
-    bv.add_function(0, plat=platform)
-    assert len(bv.functions) == 1
-
-    result = []
-    #for block in bv.functions[0].low_level_il:
-    for block in bv.functions[0].lifted_il:
-        for il in block:
-            result.append(il2str(il))
-    result = '; '.join(result)
-
-    ret = FINALIZER
-    if result.endswith(ret):
-        result = result[0:result.index(ret)]
-    if result.endswith('; '):
-        result = result[0:-2]
-
-    return result
-
-def il_str_to_tree(ilstr):
-    result = ''
-    depth = 0
-    for c in ilstr:
-        if c == '(':
-            result += '\n'
-            depth += 1
-            result += '    '*depth
-        elif c == ')':
-            depth -= 1
-        elif c == ',':
-            result += '\n'
-            result += '    '*depth
-            pass
-        elif c == ';':
-            result += '\n'
-            depth = 0
-            result += '    '*depth
-        elif c == ' ':
-            pass
-        else:
-            result += c
-    return result
-
-# print(il_str_to_tree('foo(bar)'))
-# print(il_str_to_tree('a(b(c,d(z),e));d(e(f))'))
-# print(il_str_to_tree('LLIL_RET(LLIL_POP.d())' + ';LLIL_RET(LLIL_POP.d())'))
-
-def test_all():
-    ret = True
-    for (test_i, (data, expected)) in enumerate(test_cases):
-        actual = instr_to_il(data)
-        if actual != expected:
-            print('MISMATCH AT TEST %d!' % test_i)
-            print('\t   input: %s' % data.hex())
-            print('\texpected: %s' % expected)
-            # print(il_str_to_tree(expected))
-            print('\t  actual: ')
-            print(actual)
-            print('\t    tree:')
-            print(il_str_to_tree(actual))
-            print('\n\n')
-            ret = False
-    return ret
